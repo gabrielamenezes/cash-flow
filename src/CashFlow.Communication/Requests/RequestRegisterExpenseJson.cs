f3be598bd63374411cdd0dbae1a31687e3334d68
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-29 14:22:22 -0300

    refactor: removing assignments

--- src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -5,7 +5,7 @@ public class RequestRegisterExpenseJson
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public DateTime Date { get; set; } = default;
-    public decimal Amount { get; set; } = default;
-    public PaymentType PaymentType { get; set; } = default;
+    public DateTime Date { get; set; } 
+    public decimal Amount { get; set; } 
+    public PaymentType PaymentType { get; set; }
 }
