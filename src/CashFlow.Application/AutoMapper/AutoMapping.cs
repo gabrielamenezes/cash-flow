a2f7fa9b9ddc5516ff7090557ad70a278562919e
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-31 15:01:36 -0300

    Add AutoMapper integration for request and response mapping
    Created AutoMapping.cs to define mappings between request and entity objects (RequestRegisterExpenseJson to Expense) and between entity and response objects (Expense to ResponseRegisteredExpensiveJson). Updated CashFlow.Application.csproj to include AutoMapper package reference (v13.0.1). Modified DependencyInjectionExtension.cs to register AutoMapping profile with the dependency injection container and updated AddApplication method to call AddAutoMapper.

---
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
+
+namespace CashFlow.Application.AutoMapper;
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToEntity();
+        EntityToResponse();
+    }
+    //mapeamento de requisição para entidade a ser persistida
+    private void RequestToEntity()
+    {
+        CreateMap<RequestRegisterExpenseJson, Expense>();
+    }
+    // mapeamento de entidade para resposta
+    private void EntityToResponse()
+    {
+        CreateMap<Expense, ResponseRegisteredExpensiveJson>();
+    }
+}
