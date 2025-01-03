3a22e3c98c14bcfc175e675515afa15320c970ce
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-02 21:23:57 -0300

    Remove IUnitOfWork from GetExpenseByIdUseCase

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -5,10 +5,9 @@ using CashFlow.Domain.Repositories;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById;
 
-public class GetExpenseByIdUseCase(IExpensesRepository expensesRepository, IUnitOfWork unitOfWork, IMapper mapper) : IGetExpenseByIdUseCase
+public class GetExpenseByIdUseCase(IExpensesRepository expensesRepository, IMapper mapper) : IGetExpenseByIdUseCase
 {
     private readonly IExpensesRepository _expensesRepository = expensesRepository;
-    private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly IMapper _mapper = mapper;
     public Task<ResponseExpenseJson> Execute(long id)
     {
