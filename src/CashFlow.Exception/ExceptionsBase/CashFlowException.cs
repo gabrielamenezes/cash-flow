7accb83703dd4dea102e3313e3cae22a343db9b0
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-04 17:31:09 -0300

    refactor: apply SOLID's Open/Closed Principle to enhance exception handling and filters

--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -4,4 +4,7 @@ public abstract class CashFlowException : SystemException
     protected CashFlowException(string message) : base(message)
     {
     }
+
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErros();
 }
