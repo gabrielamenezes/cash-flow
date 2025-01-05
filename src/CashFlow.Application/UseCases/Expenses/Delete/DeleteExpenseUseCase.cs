9393481a3a172885266613b1edce76e195c8fe1a
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-05 15:26:44 -0300

    Add use case for deleting expenses
    Introduce DeleteExpenseUseCase to handle expense deletion logic.
    Implement IDeleteExpenseUseCase interface with Execute method.
    Handle NotFoundException for non-existent expenses.
    Commit transaction upon successful deletion.

---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete;
+
+public class DeleteExpenseUseCase(IExpensesWriteOnlyRepository expensesRepository, IUnitOfWork unitOfWork) : IDeleteExpenseUseCase
+{
+    private readonly IExpensesWriteOnlyRepository _expensesRepository = expensesRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    public async Task Execute(long id)
+    {
+        var result = await _expensesRepository.Delete(id);
+        if (!result)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+        await _unitOfWork.Commmit();
+    }
+}
