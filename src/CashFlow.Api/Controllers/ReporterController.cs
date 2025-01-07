8a9af3654ec7ca0b2fda576a13b21a6fc7488cab
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-07 19:13:15 -0300

    feature: add reporter controller to get a excel archive

---
+++ src/CashFlow.Api/Controllers/ReporterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ReporterController : ControllerBase
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExcel()
+    {
+        byte[] file = new byte[1];
+        if(file.Length == 0)
+        {
+            return NoContent();
+        }
+        return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+    }
+}
