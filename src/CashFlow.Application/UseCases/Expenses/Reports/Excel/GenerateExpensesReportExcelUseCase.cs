1f1f314d138c3be4dfdb11d672f4cdf59dae8b65
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-08 19:42:49 -0300

    feat: add UseCase to generate report excel and add to DI service

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,9 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        return new byte[1];
+    }
+}
