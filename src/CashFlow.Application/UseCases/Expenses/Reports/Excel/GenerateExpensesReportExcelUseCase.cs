4737498462617a2c9d393c75e9fd454e2ea8853c
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-09 20:10:34 -0300

    Refactor Excel report generation in Expenses module
    Updated namespace to include necessary dependencies.
    Modified Execute method to use ClosedXML for Excel creation.
    Configured workbook with author, font size, and font name.
    Added "Despesas" worksheet to the workbook.
    Introduced InsertHeader method for header row styling.
    Styled header cells with bold text, background color, and alignment.

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,9 +1,39 @@
-﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+﻿using CashFlow.Domain.Reports;
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
     public async Task<byte[]> Execute(DateOnly month)
     {
-        return new byte[1];
+        var workbook = new XLWorkbook(); //gerando um arquivo em branco
+        workbook.Author = "Gabriela Menezes";
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Times New Roman";
+
+        month.ToString("Y");
+
+        var worksheet = workbook.Worksheets.Add("Despesas");
+        InsertHeader(worksheet);
+    }
+
+    private void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourceReportGenerationMessage.TITLE;
+        worksheet.Cell("B1").Value = ResourceReportGenerationMessage.DATE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessage.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessage.AMOUNT;
+        worksheet.Cell("E1").Value = ResourceReportGenerationMessage.DESCRIPTION;
+
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#OC65EE");
+
+        //centralizando o texto
+        worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center); 
+        worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center); 
+        worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center); 
+        worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center); 
+        worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right); 
     }
 }
