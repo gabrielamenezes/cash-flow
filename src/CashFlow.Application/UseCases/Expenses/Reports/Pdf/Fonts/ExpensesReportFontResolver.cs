b007d097da09986efef7438b6bbe210eb6fd3dea
Gabriela Menezes <gabimenezess10@gmail.com>
2025-03-15 09:36:36 -0300

    Implement font retrieval in ExpensesReportFontResolver
    Updated ExpensesReportFontResolver to implement GetFont method,
    which now reads the specified font file or falls back to a default
    font. Added a private ReadFontFile method to retrieve font files
    from manifest resources. Introduced DEFAULT_FONT constant in
    FontHelper.cs for fallback font.

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,16 +1,32 @@
 ﻿using PdfSharp.Charting;
 using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName) ?? ReadFontFile(FontHelper.DEFAULT_FONT);
+        var length = (int)stream!.Length;
+        var data = new byte[length];
+        stream.Read(buffer: data, offset: 0, length);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        // tudo que é necessário pro projeto funcionar vai estar aqui
+        // pedindo a plataforma do .net, para devolver a referencia do projeto que está sendo executado agora
+        var assembly = Assembly.GetExecutingAssembly();
+        // vou ler como stream esse arquivo de faceName
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,6 +1,7 @@
 ﻿
 using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
@@ -12,6 +13,7 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
     public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
     {
         _repository = repository;
+        // ensinando a nossa biblioteca a como resolver as fontes
         GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
     }
 
@@ -23,6 +25,17 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
             return [];
         }
 
+        var document = CreateDocument(month);
+
         return [];
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        document.Info.Title = "Relatório de Despesas";
+        document.Info.Author = "Gabriela Menezes";
+        document.Info.Subject = $"Relatório de Despesas do mês de {month:Y}";
+        return document;
     }
 }
