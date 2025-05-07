7859a44b66fa6c7b6351478c20ef3c06aed1abb4
gabrielamenezes <gabimenezess10@gmail.com>
2025-05-07 16:33:58 -0300

    feat: fix color constant name and enhance report styling

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,4 +1,5 @@
 ﻿
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Colors;
 using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
@@ -36,6 +37,23 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         foreach (var expense in expenses)
         {
             var table = CreateExpenseTable(page);
+            var row = table.AddRow();
+            row.Height = 25;
+            row.Cells[0].AddParagraph(expense.Title);
+            row.Cells[0].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 14, Color = ColorsHelper.BLACK };
+            row.Cells[0].Shading.Color = ColorsHelper.RED_LIGHT;
+            row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+            row.Cells[0].MergeRight = 2;
+            row.Cells[0].Format.LeftIndent = 20;
+
+            row.Cells[3].AddParagraph(ResourceReportGenerationMessage.AMOUNT);
+            row.Cells[3].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 14, Color = ColorsHelper.WHITE };
+            row.Cells[3].Shading.Color = ColorsHelper.RED_DARK;
+            row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+
+            row = table.AddRow();
+            row.Height = 30;
+            row.Borders.Visible = false;
         }
 
         return RenderDocument(document);
